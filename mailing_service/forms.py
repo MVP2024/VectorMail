d8da3b64961fdc1c04922b2c5f84511cf614f5c1
Medvedevy <walery.medvedev@gmail.com>
2025-06-11 16:20:00 +0300

    Устранено предупреждение PyCharm в forms.py

--- mailing_service/forms.py
+++ mailing_service/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 from .models import Mailing
+from typing import cast
+from django.db.models.query import QuerySet
+
 
 class MailingSendForm(forms.Form):
     mailing = forms.ModelChoiceField(
-        queryset=Mailing.objects.all(),
+        queryset=cast(QuerySet, Mailing.objects.all()),
         label="Выберите рассылку для отправки",
         empty_label="--- Выберите рассылку ---"
     )
